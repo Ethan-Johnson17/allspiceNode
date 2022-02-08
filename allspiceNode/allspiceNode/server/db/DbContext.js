1a1f20a10fae0c8c9a9b80c017ff3f0df8490811
Ethan Johnson <ethan.t.johnson1417@gmail.com>
2022-02-08 15:20:53 -0700

    Recipes service

--- allspiceNode/allspiceNode/server/db/DbContext.js
+++ allspiceNode/allspiceNode/server/db/DbContext.js
@@ -1,11 +1,19 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
-import { ValueSchema } from '../models/Value'
+import { RecipeSchema } from '../models/Recipe'
+import { IngredientSchema } from '../models/Ingredient'
+import { StepSchema } from '../models/Step'
+import { FavoriteSchema } from '../models/Favorite'
+import { TrySchema } from '../models/Try'
 
 class DbContext {
-  Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
+  Recipe = mongoose.model('Recipe', RecipeSchema)
+  Ingredient = mongoose.model('Ingredient', IngredientSchema)
+  Step = mongoose.model('Step', StepSchema)
+  Favorite = mongoose.model('Favorite', FavoriteSchema)
+  Try = mongoose.model('Try', TrySchema)
 }
 
 export const dbContext = new DbContext()
--- allspiceNode/allspiceNode/server/services/RecipesService.js
+++ allspiceNode/allspiceNode/server/services/RecipesService.js
@@ -1,22 +1,42 @@
+import { dbContext } from '../db/DbContext'
+import { BadRequest, Forbidden } from '../utils/Errors'
+
 class RecipesService {
-  getAll(query) {
-    throw new Error('Method not implemented.')
+  async getAll(query = {}) {
+    const recipes = await dbContext.Recipe.find(query)
+    return recipes
   }
 
-  getById(id) {
-    throw new Error('Method not implemented.')
+  async getById(id) {
+    const recipe = await dbContext.Recipe.findById(id).populate('account')
+    if (!recipe) {
+      throw new BadRequest('Cannot find that recipe')
+    }
+    return recipe
   }
 
-  create(body) {
-    throw new Error('Method not implemented.')
+  async create(body) {
+    const recipe = await dbContext.Recipe.create(body)
+    return recipe.populate('account')
   }
 
-  edit(body) {
-    throw new Error('Method not implemented.')
+  async edit(body) {
+    const recipe = await this.getById(body.id)
+    if (recipe.creatorId.toString() !== body.creatorId) {
+      throw new Forbidden("Play nice, that's not yours.")
+    }
+    const myRecipe = await dbContext.Recipe.findOneAndUpdate({ _id: body.id, creatorId: body.creatorId }, body, { new: true })
+    return myRecipe
   }
 
-  delete(recipeId, userId) {
-    throw new Error('Method not implemented.')
+  async delete(recipeId, userId) {
+    const recipe = await this.getById(recipeId)
+    if (recipe.creatorId.toString() !== userId) {
+      throw new Forbidden('Access Denied')
+    }
+    await dbContext.Recipe.findByIdAndDelete(recipeId)
+    await dbContext.Favorite.deleteMany({ recipeId: recipeId, creatorId: userId })
+    await dbContext.Try.deleteMany({ recipeId: recipeId, creatorId: userId })
   }
 }
 export const recipesService = new RecipesService()
